8f800cc5eca68f9e5cc479c5671283fefb2440bb
Resty Nasimbwa <restynasimbwa@Restys-MBP.lan>
2026-04-25 23:30:53 -0500

    css-styling

---
+++ newsgrid/src/components/header.jsx
@@ -0,0 +1,15 @@
+
+
+const getTheYear = () =>{
+    const newDate = new Date()
+    return newDate.getFullYear()
+}
+const Header = () => {
+    return(<header >
+                <div className='logo'> LOGO</div>
+                <input/>
+            </header>
+    )
+
+}
+export default Header;
--- newsgrid/src/main.jsx
+++ newsgrid/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import Header from './components/header'
+import './styles/styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const App = () => (
+    //React.Fragment - a way to bundle elements without using dev
+    // use className  if on a jsx file
+    <>
+        <Header/>
+    </>
 )
+
+createRoot(document.getElementById('root')).render(<App/>);;
